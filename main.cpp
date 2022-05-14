a23e9d1f3aff8ce99adfa0c9da5595c28fa56fd0
Devin Kongdehong <920065391@qq.com>
2022-05-14 15:58:58 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -319,28 +319,31 @@ string iteration(string str) {//迭代压缩函数实现
 }
 
 int main() {//主函数
-	string str[2];
-	str[0] = "abc";
-	str[1] = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";
-	for (int num = 0; num < 2; num++) {
-		cout << "示例 " + to_string(num + 1) + " ：输入消息为字符串: " + str[num] << endl;
-		cout << endl;
-		string paddingValue = padding(str[num]);
-		cout << "填充后的消息为：" << endl;
-		for (int i = 0; i < paddingValue.size() / 64; i++) {
-			for (int j = 0; j < 8; j++) {
-				cout << paddingValue.substr(i * 64 + j * 8, 8) << "  ";
-			}
-			cout << endl;
+	string attack = "123";
+	string str = "abc";
+	cout << "输入消息为字符串: " + str << endl;
+	cout << endl;
+	string paddingValue = padding(str);
+	cout << "填充后的消息为：" << endl;
+	for (int i = 0; i < paddingValue.size() / 64; i++) {
+		for (int j = 0; j < 8; j++) {
+			cout << paddingValue.substr(i * 64 + j * 8, 8) << "  ";
 		}
-		cout << endl;
-		string result = iteration(paddingValue);
-		cout << "杂凑值：" << endl;
-		for (int i = 0; i < 8; i++) {
-			cout << result.substr(i * 8, 8) << "  ";
-		}
-		cout << endl;
 		cout << endl;
 	}
+	cout << endl;
+	string result = iteration(paddingValue);
+	cout << "杂凑值：" << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << result.substr(i * 8, 8) << "  ";
+	}
+	string paddingAttack=padding(attack);
+	string resultAttack = iteration(paddingAttack);
+	cout << "长度扩展攻击后的杂凑值：" << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << resultAttack.substr(i * 8, 8) << "  ";
+	}
+	cout << endl;
+	cout << endl;
 }
 
